eea11fd86dbae6fa1e885b2e97f390f7eecd32cc
riteshbenjwal <riteshbenjwal7@gmail.com>
2024-12-05 17:13:38 +0530

    add job service

---
+++ apps/jobber-jobs/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { JobsModule } from './jobs/jobs.module';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    ConfigModule,
+    JobsModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
