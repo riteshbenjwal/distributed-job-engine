55c3f3c007c104c4342b04dbe01c03ccf2c730c1
riteshbenjwal <riteshbenjwal7@gmail.com>
2024-12-09 16:31:15 +0530

    add grpc auth connectivity

--- apps/jobber-jobs/src/app/jobs/jobs.module.ts
+++ apps/jobber-jobs/src/app/jobs/jobs.module.ts
@@ -1,11 +1,26 @@
+import { FibonacciJob } from './fibonacci.job';
 import { Module } from '@nestjs/common';
 import { DiscoveryModule } from '@golevelup/nestjs-discovery';
-import { FibonacciJob } from './fibonacci.job';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { JobsService } from './jobs.service';
 import { JobsResolver } from './jobs.resolver';
+import { join } from 'path';
+import { AUTH_PACKAGE_NAME } from 'types/auth';
 
 @Module({
-  imports: [DiscoveryModule],
+  imports: [
+    DiscoveryModule,
+    ClientsModule.register([
+      {
+        name: AUTH_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: AUTH_PACKAGE_NAME,
+          protoPath: join(__dirname, 'proto/auth.proto'),
+        },
+      },
+    ]),
+  ],
   providers: [FibonacciJob, JobsService, JobsResolver],
 })
 export class JobsModule {}
